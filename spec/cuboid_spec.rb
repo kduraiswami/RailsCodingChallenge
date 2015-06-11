408c771fa2ae7a7a7234b1db05b628b550a09d9d
Kailash <kaidurai@gmail.com>
2015-06-11 11:16:16 -0700

    changed variable name from cuboid to cuboid_mover to prevent errors

--- spec/cuboid_spec.rb
+++ spec/cuboid_spec.rb
@@ -81,49 +81,49 @@ describe Cuboid do
 
 		before (:each) do 
 			@cuboid_mover = Cuboid.new(length: 4, width: 4, height: 4) 
-			@cuboid.move_to!(2,2,2)
+			@cuboid_mover.move_to!(2,2,2)
 		end
 
 		it "should change the origin to (2,2,2)" do 
-			expect(@cuboid.origin).to eq([2,2,2])
+			expect(@cuboid_mover.origin).to eq([2,2,2])
 		end
 
 		#top face
 		it "should have a closest top left verticy of [0,0,4]" do
-			expect(@cuboid_vertices.vertices[0]).to match_array([0,0,4])
+			expect(@cuboid_mover.vertices[0]).to match_array([0,0,4])
 		end
 
 		it "should have a furthest top left verticy of [0,4,4]" do
-			expect(@cuboid_vertices.vertices[1]).to match_array([0,4,4])
+			expect(@cuboid_mover.vertices[1]).to match_array([0,4,4])
 		end
 
 		it "should have a furthest top right verticy of [4,4,4]" do
-			expect(@cuboid_vertices.vertices[2]).to match_array([4,4,4])
+			expect(@cuboid_mover.vertices[2]).to match_array([4,4,4])
 		end
 
 		it "should have a closest top right verticy of [4,0,4]" do
-			expect(@cuboid_vertices.vertices[3]).to match_array([4,0,4])
+			expect(@cuboid_mover.vertices[3]).to match_array([4,0,4])
 		end
 
 		#bottom face
 		it "should have a closest bottom left verticy of [0,0,0]" do
-			expect(@cuboid_vertices.vertices[4]).to match_array([0,0,0])
+			expect(@cuboid_mover.vertices[4]).to match_array([0,0,0])
 		end
 
 		it "should have a furthest bottom left verticy of [0,4,0]" do
-			expect(@cuboid_vertices.vertices[5]).to match_array([0,4,0])
+			expect(@cuboid_mover.vertices[5]).to match_array([0,4,0])
 		end
 
 		it "should have a furthest bottom right verticy of [4,4,0]" do
-			expect(@cuboid_vertices.vertices[6]).to match_array([4,4,0])
+			expect(@cuboid_mover.vertices[6]).to match_array([4,4,0])
 		end
 
 		it "should have a closest bottom right verticy of [4,0,0]" do
-			expect(@cuboid_vertices.vertices[7]).to match_array([4,0,0])
+			expect(@cuboid_mover.vertices[7]).to match_array([4,0,0])
 		end
 
 		it "changes the origin in the simple happy case" do
-			expect(subject.move_to!(1,2,3)).to be true
+			expect(@cuboid_mover.move_to!(1,2,3)).to be true
 		end
 	end    
   
