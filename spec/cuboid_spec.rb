be5a5bb07429e3e2268e03e8db31532c40107a5a
Kailash <kaidurai@gmail.com>
2015-06-11 10:13:51 -0700

    complete tests for initialize function

--- spec/cuboid_spec.rb
+++ spec/cuboid_spec.rb
@@ -4,12 +4,45 @@ require 'cuboid'
 #  Do whatever you need to do to make it work and please add your own test cases for as many
 #  methods as you feel need coverage
 describe Cuboid do
- 
-  describe "move_to" do
-    it "changes the origin in the simple happy case" do
-      expect(subject.move_to!(1,2,3)).to be true
-    end
-  end    
+	
+	describe "initialize" do 
+		before (:each) do 
+			@cuboid_one = Cuboid.new(length: 3, width: 3, height: 4)
+		end
+
+		it "should have a length property" do 
+			(@cuboid_one.length).to eq(3)
+		end
+
+		it "should have a width property" do 
+			(@cuboid_one.width).to eq(3)
+		end
+
+		it "should have a height property" do 
+			(@cuboid_one.height).to eq(4)
+		end
+
+		it "should have a default origin at center" do 
+			(@cuboid_one.origin).to match_array([0,0,0])
+		end
+
+		it "should accept custom origin" do 
+			@cuboid_two = Cuboid.new(length: 3, width: 3, height: 4, origin: [3,3,3])
+			(@cuboid_two.origin).to match_array([3,3,3])
+		end
+
+		# it "should have eight verticies" do 
+		# 	{@cuboid_one.verticies.length}.to eq(8)
+		# end
+
+	end
+
+
+	describe "move_to" do
+		it "changes the origin in the simple happy case" do
+			expect(subject.move_to!(1,2,3)).to be true
+		end
+	end    
   
   describe "intersects?" do
   end
