c1216fd453f04da62f8cd3a2fec0526cdb5b9265
Kailash <kaidurai@gmail.com>
2015-06-11 10:55:58 -0700

    completed verticy creation logic

--- lib/cuboid.rb
+++ lib/cuboid.rb
@@ -1,16 +1,28 @@
 
 class Cuboid
 
-	attr_accessor :length, :width, :height, :origin
+	attr_accessor :length, :width, :height, :origin, :vertices
 	
 	def initialize(dimensions={})
 		@length = dimensions.fetch(:length)
 		@width = dimensions.fetch(:width)
 		@height = dimensions.fetch(:height)
 		@origin = dimensions.fetch(:origin) {[0,0,0]}
+		@vertices = []
+		make_vertices
 	end	
 
-	def vertices
+	def make_vertices
+		#starts from closest top left corner and goes around clockwise [x,y,z] coordinates
+		@vertices.insert(0, [(@width/-2), (@length/-2), (@height/2)])
+		@vertices.insert(1, [(@width/-2), (@length/2), (@height/2)])
+		@vertices.insert(2, [(@width/2), (@length/2), (@height/2)])
+		@vertices.insert(3, [(@width/2), (@length/-2), (@height/2)])
+		#starts from closet bottom left corner and goes around clockwise [x,y,z] coordinates
+		@vertices.insert(4, [(@width/-2), (@length/-2), (@height/-2)])
+		@vertices.insert(5, [(@width/-2), (@length/2), (@height/-2)])
+		@vertices.insert(6, [(@width/2), (@length/2), (@height/-2)])
+		@vertices.insert(7, [(@width/2), (@length/-2), (@height/-2)])
 	end
 
 	def move_to!(x, y, z)
