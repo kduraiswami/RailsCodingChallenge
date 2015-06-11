7e2604470f5e6519e0228a1d14941087ed77377a
Kailash <kaidurai@gmail.com>
2015-06-11 14:35:39 -0700

    completed intersection logic

--- lib/cuboid.rb
+++ lib/cuboid.rb
@@ -32,7 +32,36 @@ class Cuboid
 		@vertices.insert(7, [(@width/2)+@origin[0], (@length/-2)+@origin[1], (@height/-2)+@origin[2]])
 	end
 
-	def intersects?(other)
+	def compare_vertices(vertices, potential_conflict_vertices)
+		#check if left face is to the left of conflict right face
+		if vertices[0][0] <= potential_conflict_vertices[3][0] && vertices[0][0] >= potential_conflict_vertices[0][0]
+			false
+		#check if right face is to the right of conflict left face
+		elsif vertices[3][0] >= potential_conflict_vertices[0][0] && vertices[3][0] >= potential_conflict_vertices[3][0]
+			false
+		#check if close face is further than conflict close face
+		elsif vertices[0][1] >= potential_conflict_vertices[0][1] && vertices[1][1] <= potential_conflict_vertices[1][1]
+			false
+		#check if further face is closer than conflict further face
+		elsif vertices[1][1] <= potential_conflict_vertices[1][1] && vertices[1][1] >= potential_conflict_vertices[0][1]
+			false
+		#check if top face is above conflict bottom face
+		elsif vertices[0][2] >= potential_conflict_vertices[4][2] && vertices[0][2] <= potential_conflict_vertices[0][2]
+			false
+		#check if bottom face is below conflict top face
+		elsif vertices[4][2] <= potential_conflict_vertices[0][2] && vertices[4][2] >= potential_conflict_vertices[4][2]
+			false
+		else
+			true
+		end
+	end
+
+	def does_not_intersect?(potential_conflict_vertices)
+		if compare_vertices(@vertices, potential_conflict_vertices)
+			true
+		else 
+			false 
+		end
 	end
 
 end
