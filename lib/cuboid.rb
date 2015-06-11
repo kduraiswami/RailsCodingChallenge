63bd431e48e2413c41fa93074b89b017cb6fda32
Kailash <kaidurai@gmail.com>
2015-06-11 11:19:24 -0700

    completed move logic function and tested if vertices change corectly

--- lib/cuboid.rb
+++ lib/cuboid.rb
@@ -12,28 +12,24 @@ class Cuboid
 		make_vertices
 	end	
 
-	def make_vertices
-		#starts from closest top left corner and goes around clockwise [x,y,z] coordinates
-		@vertices.insert(0, [(@width/-2), (@length/-2), (@height/2)])
-		@vertices.insert(1, [(@width/-2), (@length/2), (@height/2)])
-		@vertices.insert(2, [(@width/2), (@length/2), (@height/2)])
-		@vertices.insert(3, [(@width/2), (@length/-2), (@height/2)])
-		#starts from closet bottom left corner and goes around clockwise [x,y,z] coordinates
-		@vertices.insert(4, [(@width/-2), (@length/-2), (@height/-2)])
-		@vertices.insert(5, [(@width/-2), (@length/2), (@height/-2)])
-		@vertices.insert(6, [(@width/2), (@length/2), (@height/-2)])
-		@vertices.insert(7, [(@width/2), (@length/-2), (@height/-2)])
-	end
-
 	def move_to!(x, y, z)
 		@origin[0] = x
 		@origin[1] = y
 		@origin[2] = z
-		change_vertices
+		make_vertices
 	end
 
-	def change_vertices
-		
+	def make_vertices
+		#starts from closest top left corner and goes around clockwise [x,y,z] coordinates
+		@vertices.insert(0, [(@width/-2)+@origin[0], (@length/-2)+@origin[1], (@height/2)+@origin[2]])
+		@vertices.insert(1, [(@width/-2)+@origin[0], (@length/2)+@origin[1], (@height/2)+@origin[2]])
+		@vertices.insert(2, [(@width/2)+@origin[0], (@length/2)+@origin[1], (@height/2)+@origin[2]])
+		@vertices.insert(3, [(@width/2)+@origin[0], (@length/-2)+@origin[1], (@height/2)+@origin[2]])
+		#starts from closet bottom left corner and goes around clockwise [x,y,z] coordinates
+		@vertices.insert(4, [(@width/-2)+@origin[0], (@length/-2)+@origin[1], (@height/-2)+@origin[2]])
+		@vertices.insert(5, [(@width/-2)+@origin[0], (@length/2)+@origin[1], (@height/-2)+@origin[2]])
+		@vertices.insert(6, [(@width/2)+@origin[0], (@length/2)+@origin[1], (@height/-2)+@origin[2]])
+		@vertices.insert(7, [(@width/2)+@origin[0], (@length/-2)+@origin[1], (@height/-2)+@origin[2]])
 	end
 
 	def intersects?(other)
